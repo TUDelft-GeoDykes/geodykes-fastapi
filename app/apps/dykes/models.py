eeb14a48a44d01fac6c0251983a6c1f5a8a24ae7
Selin Kubilay <d.s.kubilay@tudelft.nl>
2024-06-18 15:11:25 +0200

    Update models.py to add the timeseries relationship

--- app/apps/dykes/models.py
+++ app/apps/dykes/models.py
@@ -28,6 +28,8 @@ class Crossection(BaseModel):
     name = sa.Column(sa.String, nullable=False)  # Name or identifier of the crossection
     description = sa.Column(sa.String, nullable=True)  # Optional detailed description of the crossection
     topology = sa.Column(sa.String, nullable=False)  # Descriptive attribute for the shape or structure
+    # Relationship to Timeseries, indicating one crossection can have multiple timeseries.
+    timeseries = relationship("Timeseries", backref="crossection")
 
 # Timeseries model represents timestampes of a certain unit
 class Timeseries(BaseModel):
