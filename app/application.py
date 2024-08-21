73ecea767509616a38f29ee1fdcaed8158bde3ad
jurra <jose.urra86@gmail.com>
2024-08-21 15:33:40 +0200

    Removed sample app from Geodykes app

--- app/application.py
+++ app/application.py
@@ -5,7 +5,6 @@ load_dotenv()  # This line must come before importing any modules that use envir
 from fastapi import Depends, FastAPI
 
 from app import exceptions
-from app.apps.decks.views import router as decks_router
 from app.apps.dykes.views import router as dykes_router
 from app.db.deps import set_db
 from app.db.exceptions import DatabaseValidationError
@@ -19,7 +18,6 @@ def get_app() -> FastAPI:
         dependencies=[Depends(set_db)],
     )
 
-    _app.include_router(decks_router, prefix="/api")
     _app.include_router(dykes_router, prefix="/api")
 
     _app.add_exception_handler(
